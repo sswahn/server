deffbf0e0f0a755a762ebf5dc10599dfce3ae70d
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-18 17:55:57 -0400

    Update server.js

--- src/core/server.js
+++ src/core/server.js
@@ -1,17 +1,9 @@
-export const get = api => {
+export const create = async (api, request) => {
   if (typeof api !== 'string') {
-    throw new Error('Get request expects argument of type string.')
-  }
-  const response = fetch(api)
-  return response.json()
-}
-
-export const post = async (api, request) => {
-  if (typeof api !== 'string') {
-    throw new Error('Get request expects first argument of type string.')
+    throw new Error('Post request expects first argument of type string.')
   }
   if (typeof request !== 'object' || Array.isArray(request)) {
-    throw new Error('Get request expects second argument of type object literal.')
+    throw new Error('Post request expects second argument of type object literal.')
   }
   const response = fetch(api, {
     type: 'post',
@@ -23,3 +15,40 @@ export const post = async (api, request) => {
   return response.json()
 }
 
+export const read = api => {
+  if (typeof api !== 'string') {
+    throw new Error('Get request expects argument of type string.')
+  }
+  const response = fetch(api)
+  return response.json()
+}
+
+export const update = async (api, request) => {
+  if (typeof api !== 'string') {
+    throw new Error('Put request expects first argument of type string.')
+  }
+  if (typeof request !== 'object' || Array.isArray(request)) {
+    throw new Error('Put request expects second argument of type object literal.')
+  }
+  const response = fetch(api, {
+    type: 'put',
+    body: JSON.stringify(request),
+    header: {
+      'Content-Type': 'application/json'
+    }
+  })
+  return response.json()
+}
+
+export const remove = async api => {
+  if (typeof api !== 'string') {
+    throw new Error('Delete request expects first argument of type string.')
+  }
+  const response = fetch(api, {
+    type: 'delete',
+    header: {
+      'Content-Type': 'application/json'
+    }
+  })
+  return response.json()
+}
