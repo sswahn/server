4577294ef4357d6e131c2a91083b4470c7b1f215
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-24 21:51:15 -0400

    Update server.js

--- src/core/server.js
+++ src/core/server.js
@@ -1,54 +1,76 @@
+const typeCheck = (method, api, request = undefined, headers = {}) => {
+  if (typeof api !== 'string') {
+    throw new Error(`${method} request expects first argument to be of type string.`)
+  }
+  if (request && (typeof request !== 'object' || Array.isArray(request))) {
+    throw new Error(`${method} request expects second argument to be of type object.`)
+  }
+  if (typeof headers !== 'object' || Array.isArray(headers)) {
+    throw new Error(`${method} request expects headers to be an object.`)
+  }
+}
+
 const server = {
-  async get(api) {
-    if (typeof api !== 'string') {
-      throw new Error('Get request expects argument of type string.')
+  async get(api, headers = {}) {
+    try {
+      typeCheck('get', api, undefined, headers)
+      const response = await fetch(api, {
+        headers: {
+          'Content-Type': 'application/json',
+          ...headers
+        }
+      })
+      return response.json()
+    } catch (error) {
+      throw new Error(`Failed to execute GET request. Error: ${error}`)
     }
-    const response = await fetch(api)
-    return response.json()
   },
-  async post(api, request) {
-    if (typeof api !== 'string') {
-      throw new Error('Post request expects first argument of type string.')
+  async post(api, request, headers = {}) {
+    try {
+      typeCheck('post', api, request, headers)
+      const response = await fetch(api, {
+        method: 'post',
+        body: JSON.stringify(request),
+        headers: {
+          'Content-Type': 'application/json',
+          ...headers
+        }
+      })
+      return response.json()
+    } catch (error) {
+      throw new Error(`Failed to execute POST request. Error: ${error}`)
     }
-    if (typeof request !== 'object' || Array.isArray(request)) {
-      throw new Error('Post request expects second argument of type object.')
-    }
-    const response = await fetch(api, {
-      method: 'post',
-      body: JSON.stringify(request),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    return response.json()
   },
-  async put(api, request) {
-    if (typeof api !== 'string') {
-      throw new Error('Put request expects first argument of type string.')
+  async put(api, request, headers = {}) {
+    try {
+      typeCheck('put', api, request, headers)
+      const response = await fetch(api, {
+        method: 'put',
+        body: JSON.stringify(request),
+        headers: {
+          'Content-Type': 'application/json',
+          ...headers
+        }
+      })
+      return response.json()
+    } catch (error) {
+      throw new Error(`Failed to execute PUT request. Error: ${error}`)
     }
-    if (typeof request !== 'object' || Array.isArray(request)) {
-      throw new Error('Put request expects second argument of type object.')
-    }
-    const response = await fetch(api, {
-      method: 'put',
-      body: JSON.stringify(request),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    return response.json()
   },
-  async delete(api) {
-    if (typeof api !== 'string') {
-      throw new Error('Delete request expects first argument of type string.')
+  async delete(api, headers = {}) {
+    try {
+      typeCheck('delete', api, undefined, headers)
+      const response = await fetch(api, {
+        method: 'delete',
+        headers: {
+          'Content-Type': 'application/json',
+          ...headers
+        }
+      })
+      return response.json()
+    } catch (error) {
+      throw new Error(`Failed to execute DELETE request. Error: ${error}`)
     }
-    const response = await fetch(api, {
-      method: 'delete',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    return response.json()
   }
 }
 
